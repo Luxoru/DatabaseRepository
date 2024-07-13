7cebd1c3c78c718304ceb5bc05295496fd0ee27f
Luxoru <luxoru.dev@gmail.com>
2024-07-13 21:11:41 +0100

    Added pattern handlin

--- src/main/java/me/luxoru/databaserepository/impl/redis/RedisMessenger.java
+++ src/main/java/me/luxoru/databaserepository/impl/redis/RedisMessenger.java
@@ -2,8 +2,16 @@ package me.luxoru.databaserepository.impl.redis;
 
 public interface RedisMessenger {
 
-    default void onMessage(String channel, String message){}
+    default void onMessage(String channel, String message) {
+    }
+
+    default void onPMessage(String pattern, String channel, String message) {
+    }
 
     String[] getChannels();
 
+
+    default boolean isUsingPatterns() {
+        return false;
+    }
 }
